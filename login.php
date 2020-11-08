0d84388567ae37ce9b0f08699aeddb8215c0da9e
Tamar-amaral <67126697+Tamar-amaral@users.noreply.github.com>
2020-11-08 18:52:30 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,56 +1,36 @@
-<!doctype html>
-<html lang="pt-br">
+<?php
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+require_once("../dao/Conecta.php");
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="./css/login.css">
+class login {
 
-  <title>Food Healthy | Login</title>
-</head>
+    public $email;
+    public $password;
+    
+}
 
-<body>
-  <div class="vertical-center">
-    <div id="login">
-      <img src="./img/login_logo.png" alt="" >
-      <form action="" >
-        <div class="form-group">
-          <label for="exampleInputEmail1" class="text-white">Email</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            placeholder="Email">
-        </div>
-        <div class="form-group" >
-          <label for="exampleInputPassword1" class="text-white">Senha</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
-        </div>
-        <a href=""></a>
-        <button type="submit" class="btn btn-success" id="env">Enviar</button>
+public function salvar () {
+    try{ 
+    $dao = new DAO();
+    $sql ="INSERT into login(login.email, login.password) 
+    VALUES (:email, :password)";
+    $stman = $dao->conecta()->prepare($sql);
+    $stman->bindParam(":email", $this->email);
+    $stman->bindParam(":password", $this->password);
+    $stman->execute();
+    echo ("Cadastrado"); }
+    catch (Exception $e){
+        throw new Exception ("erro ao cadrastar", $e->menssage()); 
 
-      </form>
-    </div>
-  </div>
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-  </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-  </script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-  </script>
-</body>
-
-<?php 
- 
+    }
+}   
 
 
-?>
-
-</html>
+public function valida(){
+    $error = "";
+    if ($this->email == ""){
+        $error .= "Email em branco. <br>";          
+    }
+if ($this->password ==""){
+    $error .= "Senha em branco. <br>";
+}
