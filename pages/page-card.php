c9f566073781564385a88b7b1941682e61efbed2
Kelwin Simões de Souza <kelwinsimoesdesouza@gmail.com>
2020-11-12 17:09:09 -0300

    alterações na pagina

--- pages/page-card.php
+++ pages/page-card.php
@@ -1,1 +1,68 @@
-<h1>Card</h1>
+<section class="container" >
+          <div class="row">
+            <div class="col-md-4 col-12">
+          <div class="card " >
+            <img src="img2/suco da fruta 2.jfif" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Sucos da fruta</h5>
+              <p class="card-text">
+                            
+              
+              <form action="/action_page.php">
+                <select class="rounded-top rounded-bottom" name="sucos">
+                  <option value="laranja">Suco de Laranja e Acerola</option>
+                  <option value="Melancia">Melancia</option>
+                  <option value="Limão">Limão</option>
+                  <option value="Maracuja">Maracuja</option>
+                  <option value="Goiaba">Goiaba</option>
+                </select>
+                <input type="submit" value="Submit">
+              </form>
+            </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-12">
+          <div class="card ">
+            <img src="img2/suco detox.jpeg " class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Sucos detox</h5>
+              <p class="card-text">
+              
+              <form action="/action_page.php">
+                <select class="rounded-top rounded-bottom" name="sucos">
+                  <option value="laranja">Suco de Laranja e Acerola</option>
+                  <option value="Melancia">Melancia</option>
+                  <option value="Limão">Limão</option>
+                  <option value="Maracuja">Maracuja</option>
+                  <option value="Goiaba">Goiaba</option>
+                </select>
+                <input type="submit" value="Submit">
+              </form>
+            </div>
+            </div>
+      
+          </div>
+          <div class="col-md-4 col-12">
+          <div class="card ">
+            <img src="img2/sucos funcionais.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Vitaminas de fruta</h5>
+              <p class="card-text">
+              
+              <form action="/action_page.php">
+                <select class="rounded-top rounded-bottom" name="sucos">
+                  <option value="laranja">Suco de Laranja e Acerola</option>
+                  <option value="Melancia">Melancia</option>
+                  <option value="Limão">Limão</option>
+                  <option value="Maracuja">Maracuja</option>
+                  <option value="Goiaba">Goiaba</option>
+                </select>
+                <input type="submit" value="Submit">
+              </form>
+              
+            </div>
+          </div>
+          </div>
+        </div>
+        </section>
+        
--- pages/page-prato.php
+++ pages/page-prato.php
@@ -1,1 +1,36 @@
-<h1>Monte seu Prato</h1>
+<section class="container" >
+        <div class="row">
+          <div class="col-md-4 col-12">
+        <div class="card " >
+          <img src="imagens dos prts/images dos prts/prato1.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Sucos da fruta</h5>
+            <p class="card-text">
+                          
+            <a href="#" class="btn btn-primary">Ver as opções</a>
+          </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-12">
+        <div class="card ">
+          <img src="imagens dos prts/images dos prts/prato2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Sucos detox</h5>
+            <p class="card-text">
+            <a href="#" class="btn btn-primary">Ver as opções</a>
+          </div>
+          </div>
+    
+        </div>
+        <div class="col-md-4 col-12">
+        <div class="card ">
+          <img src="imagens dos prts/images dos prts/prato3.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Vitaminas de fruta</h5>
+            <p class="card-text">
+            <a href="#" class="btn btn-primary">Ver as opções</a>
+          </div>
+        </div>
+        </div>
+      </div>
+      </section>
